5be0a691ea1e6e64b19bd3f95ae1384f1cbd1258
vinibfranc <vinibfranc@gmail.com>
2019-08-06 10:15:03 -0300

    Redirect count_table to its own dir

--- scripts/04_count_features.sh
+++ scripts/04_count_features.sh
@@ -3,10 +3,12 @@ set -e
 set -u
 set -o pipefail
 
-cd ../star
+cd ..
+mkdir -p htseq
+cd star
 
 htseq-count --stranded=no --format=bam --idattr=gene_name \
             SRR1536283_control_rep1Aligned.out.bam SRR1536284_control_rep2Aligned.out.bam \
             SRR1536285_48h_rep1Aligned.out.bam SRR1536286_48h_rep2Aligned.out.bam \
             SRR1536287_144h_rep1Aligned.out.bam SRR1536288_144h_rep2Aligned.out.bam \
-            ../reference_genome/GCA_000814975.1_MAN_1.0_genomic.gtf > count_table.txt
+            ../reference_genome/GCA_000814975.1_MAN_1.0_genomic.gtf > ../htseq/count_table.txt
